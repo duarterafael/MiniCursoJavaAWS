feec505503a45ae40d759d275d2d1ac558e26fb1
duarterafael <rafaelbatistaduarte@gmail.com>
2026-01-20 21:33:25 -0300

    First commit: Project initialization

---
+++ src/main/java/cesar/school/professor_allocation/ProfessorAllocationApplication.java
@@ -0,0 +1,13 @@
+package cesar.school.professor_allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cesar/school/professor_allocation/ProfessorAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package cesar.school.professor_allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
